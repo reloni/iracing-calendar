240dd9ecd96db8506e3878ee4074a3c4efa1e37a
Anton Efimenko <reloni@ya.ru>
2021-05-15 17:09:01 +0300

    Show static html using leaf

--- Package.swift
+++ Package.swift
@@ -8,13 +8,15 @@ let package = Package(
     ],
     dependencies: [
         // 💧 A server-side Swift web framework.
-        .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0"),
+        .package(url: "https://github.com/vapor/vapor.git", .exact("4.45.2")),
+        .package(url: "https://github.com/vapor/leaf", from: "4.1.2")
     ],
     targets: [
         .target(
             name: "App",
             dependencies: [
-                .product(name: "Vapor", package: "vapor")
+                .product(name: "Vapor", package: "vapor"),
+                .product(name: "Leaf", package: "leaf")
             ],
             swiftSettings: [
                 // Enable better optimizations when building in Release configuration. Despite the use of
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,11 +1,12 @@
 import Vapor
+import Leaf
 
 func routes(_ app: Application) throws {
     app.get { req in
         return "It works!"
     }
     
-    app.get("hello") { req -> String in
-        return "Hello, world!"
+    app.get("series") { req -> EventLoopFuture<View> in
+        return req.view.render("series-table", ["name": "Leaf"])
     }
 }
