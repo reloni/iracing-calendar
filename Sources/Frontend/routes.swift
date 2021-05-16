865e967cc5a4f10a116560b497612bb70f47dd5c
Anton Efimenko <reloni@ya.ru>
2021-05-16 17:32:35 +0300

    Add all series

--- Sources/Frontend/routes.swift
+++ Sources/Frontend/routes.swift
@@ -41,17 +41,33 @@ func routes(_ app: Application) throws {
             title: "Favorite series",
             series: [
                 .init(name: "Porsche iRacing Cup", nextRace: "2 minutes", startDate: "11 May", length: "16 laps", track: "Hockenheimring Baden-Württemberg - Grand Prix"),
+                .init(name: "VRS GT Sprint Series", nextRace: "12 minutes", startDate: "11 May", length: "40 mins", track: "Okayama International Circuit - Full Course")
+            ],
+            navbarItems: [
+                .init(title: "Favorites", link: "favorite-series", isActive: true),
+                .init(title: "All series", link: "all-series", isActive: false),
+                .init(title: "Profile", link: "home", isActive: false),
+            ]
+            )
+        return req.view.render("table-view", context)
+    }
+
+    app.get("all-series") { req -> EventLoopFuture<View> in
+        let context = SeriesViewContext(
+            title: "All series",
+            series: [
+                .init(name: "Porsche iRacing Cup", nextRace: "2 minutes", startDate: "11 May", length: "16 laps", track: "Hockenheimring Baden-Württemberg - Grand Prix"),
                 .init(name: "VRS GT Sprint Series", nextRace: "12 minutes", startDate: "11 May", length: "40 mins", track: "Okayama International Circuit - Full Course"),
                 .init(name: "IMSA Michelin Pilot Challenge", nextRace: "67 minutes", startDate: "11 May", length: "30 mins", track: "Mid-Ohio Sports Car Course - Full Course"),
                 .init(name: "IMSA Hagerty iRacing Series", nextRace: "112 minutes", startDate: "11 May", length: "45 mins", track: "Mid-Ohio Sports Car Course - Full Course"),
                 .init(name: "Pure Driving School European Sprint Series", nextRace: "15 minutes", startDate: "11 May", length: "60 mins", track: "Silverstone Circuit - Grand Prix")
             ],
             navbarItems: [
-                .init(title: "Favorites", link: "favorite-series", isActive: true),
-                .init(title: "All series", link: "home", isActive: false),
+                .init(title: "Favorites", link: "favorite-series", isActive: false),
+                .init(title: "All series", link: "home", isActive: true),
                 .init(title: "Profile", link: "home", isActive: false),
             ]
             )
-        return req.view.render("favorite-series", context)
+        return req.view.render("table-view", context)
     }
 }
