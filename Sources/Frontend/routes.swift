b5210559dc5ff5e164d1a4a242add80983049c3d
Anton Efimenko <reloni@ya.ru>
2021-05-16 17:29:28 +0300

    Add navbarItems

--- Sources/Frontend/routes.swift
+++ Sources/Frontend/routes.swift
@@ -1,6 +1,12 @@
 import Vapor
 import Leaf
 
+struct NavbarItem: Codable {
+    let title: String
+    let link: String
+    let isActive: Bool
+}
+
 struct Serie: Codable {
     let name: String
     let nextRace: String
@@ -12,16 +18,22 @@ struct Serie: Codable {
 protocol ViewContext: Content {
     var title: String { get }
     var series: [Serie] { get }
+    var navbarItems: [NavbarItem] { get }
 }
 
 struct SeriesViewContext: ViewContext {
     let title: String
     let series: [Serie]
+    let navbarItems: [NavbarItem]
 }
 
 func routes(_ app: Application) throws {
-    app.get { req in
-        return "It works!"
+    app.get { req in 
+        return "Home"
+    }
+
+    app.get("home") { req in 
+        return "Home"
     }
     
     app.get("favorite-series") { req -> EventLoopFuture<View> in
@@ -33,6 +45,11 @@ func routes(_ app: Application) throws {
                 .init(name: "IMSA Michelin Pilot Challenge", nextRace: "67 minutes", startDate: "11 May", length: "30 mins", track: "Mid-Ohio Sports Car Course - Full Course"),
                 .init(name: "IMSA Hagerty iRacing Series", nextRace: "112 minutes", startDate: "11 May", length: "45 mins", track: "Mid-Ohio Sports Car Course - Full Course"),
                 .init(name: "Pure Driving School European Sprint Series", nextRace: "15 minutes", startDate: "11 May", length: "60 mins", track: "Silverstone Circuit - Grand Prix")
+            ],
+            navbarItems: [
+                .init(title: "Favorites", link: "favorite-series", isActive: true),
+                .init(title: "All series", link: "home", isActive: false),
+                .init(title: "Profile", link: "home", isActive: false),
             ]
             )
         return req.view.render("favorite-series", context)
