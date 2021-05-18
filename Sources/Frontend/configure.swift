2d301661fe46297775b843fb5ac8f4cd2fa54ba9
Anton Efimenko <reloni@ya.ru>
2021-05-18 22:46:52 +0300

    Move js script to a separate file

--- Sources/Frontend/configure.swift
+++ Sources/Frontend/configure.swift
@@ -3,8 +3,8 @@ import Leaf
 
 // configures your application
 public func configure(_ app: Application) throws {
-    // uncomment to serve files from /Public folder
-    // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
+    app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
+    
     app.leaf.cache.isEnabled = app.environment.isRelease
     
     app.views.use(.leaf)
