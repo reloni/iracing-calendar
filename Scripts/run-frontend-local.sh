ec6798f89986293d7108d994614f3995829af236
Anton Efimenko <reloni@ya.ru>
2022-04-29 22:00:55 +0300

    Rename run-frontend script

--- Scripts/run-frontend-local.sh
+++ Scripts/run-frontend-local.sh
@@ -4,6 +4,6 @@ set -e
 lsof -i :9000 -sTCP:LISTEN \
     | awk 'NR > 1 {print $2}' \
     | xargs kill -15 \
-    && export $(cat ./Sources/Frontend/.env | xargs) \
-    && (cd ./Sources/Frontend && swift run Frontend serve --port 9000 --env development --log debug)
+    # && export $(cat ./Sources/Frontend/.env | xargs) \
+    (cd ./Sources/Frontend && swift run Frontend serve --port 9000 --env local --log debug)
     # && (cd ./Sources/Frontend && swift run Frontend serve --port 9000 --env development -Xswiftc -g --log debug)
